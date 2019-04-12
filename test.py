abea2ad039fb396e78006b0bd26c206333cd624b
飞鱼 <31850165+FISHzj@users.noreply.github.com>
2019-04-12 13:56:21 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,5 @@
+from student import *
+test1=Student("zhujian","男",(1997,11,9),"15241034","1781836548@qq.com")
+test1.account(username="zhujian",password="0000000",wechatnum="sdfsd",num="15241034")
+test2=Student(parameter)
+test2.account
