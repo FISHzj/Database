3545c47dd90348b05798ff6d6dd6ae8d6677033b
飞鱼 <31850165+FISHzj@users.noreply.github.com>
2019-05-12 22:37:35 +0200

    Add files via upload

--- student.py
+++ student.py
@@ -1,4 +1,5 @@
 import datetime
+import pymongo
 from bson.objectid import ObjectId
 import re
 import bson.binary
@@ -7,11 +8,11 @@ import os
 student=Connectmdb()
 class Student:  #缺少photo
     _number=0
-    def __init__(self,name,sex,birthday,ident,email,photo='',photoback='',course=[],schoolyear=0,group=0):
+    def __init__(self,name,sex,birthday,ident,email,photo='',photoback='',schoolyear=0,group=0):
         if not (isinstance(name,str) and sex in ("男","女") and isinstance(ident,str) and isinstance(photo,str) and isinstance(photoback,str)):
             raise ValueError(name,sex,ident,photo,photoback)
-        if not (isinstance(course,list) and isinstance(schoolyear,int) and isinstance(group,int)):
-            raise  ValueError(course,schoolyear,group)
+        if not (isinstance(schoolyear,int) and isinstance(group,int)):
+            raise  ValueError(schoolyear,group)
         try:
             birth=datetime.datetime(*birthday)
         except:
@@ -20,11 +21,11 @@ class Student:  #缺少photo
             if not re.match(r"^[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+){0,4}@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+){0,4}$",email):
                 raise TypeError('Wrong type for the email!')
         if not photo.endswith(('.pdf', '.jpeg', '.jpg', '.png', '.bmp')):
-            raise TypeError('topic')
+             raise TypeError('topic')
         with open(photo, 'rb') as f:
             content = bson.binary.Binary(f.read())
             student._db.student.insert_one({'name':name,'sex':sex,'birthday':birth,'num':ident,'email':email,'photoname':f.name,'photo':content,
-                                            'course':course,'schoolyear':schoolyear,'group':group})
+                                        'schoolyear':schoolyear,'group':group})
         self._photoback=photoback
         self._id=ObjectId(student._db.student.find_one({'num':ident})['_id'])
         self._condition={'_id':self._id}
@@ -59,8 +60,16 @@ class Student:  #缺少photo
     def email(self):
         self._email = student._db.student.find_one(self._condition)['email']
         return self._email
-    def course(self):
-        self._course = student._db.student.find_one(self._condition)['course']
+    def getCourse(self):
+        #count=student._db.course.count_documents({'student':{'$student'!=[]}})
+        self._course=[]
+        for result in student._db.course.find():
+            if self._name in result['_student']:
+                self._course.append(result['name'])
+
+
+
+            #student._db.course.find_one({"student":{$in:self._name}}).skip(i)['name'
         return self._course
     def schoolyear(self):
         self._schoolyear = student._db.student.find_one(self._condition)['schoolyear']
@@ -74,24 +83,24 @@ class Student:  #缺少photo
             raise TypeError('Wrong type for the new email!')
         if student._db.student.update_one(self._condition, {'$set': {'email': email_name}}):
             return True
-    def add_course(self,**course_name):
-        self._course = student._db.student.find_one(self._condition)['course']
-        newcourse=self._course.extend(list(course_name))
-        newcourse1=list(set(newcourse))
-        student._db.student.update_one(self._condition,{'$set':{'course':newcourse1}})
-        return True
-    def delete_course(self,**course_name):
-        self._course = student._db.student.find_one(self._condition)['course']
-        if len(list(course_name))==1:
-            newcourse=self._course.remove(list(course_name)[0])
-            student._db.student.update_one(self._condition, {'$set': {'course': newcourse}})
-        else:
-            course1=self._course
-            for i in range(0,len(course1)-1):
-                for j in range(0,len(list(course_name))):
-                    if course1[i]==list(course_name)[j]:
-                        del course1[i]
-            student._db.student.update_one(self._condition, {'$set': {'course': course1}})
+    # def add_course(self,**course_name):
+    #     self._course = student._db.student.find_one(self._condition)['course']
+    #     newcourse=self._course.extend(list(course_name))
+    #     newcourse1=list(set(newcourse))
+    #     student._db.student.update_one(self._condition,{'$set':{'course':newcourse1}})
+    #     return True
+    # def delete_course(self,**course_name):
+    #     self._course = student._db.student.find_one(self._condition)['course']
+    #     if len(list(course_name))==1:
+    #         newcourse=self._course.remove(list(course_name)[0])
+    #         student._db.student.update_one(self._condition, {'$set': {'course': newcourse}})
+    #     else:
+    #         course1=self._course
+    #         for i in range(0,len(course1)-1):
+    #             for j in range(0,len(list(course_name))):
+    #                 if course1[i]==list(course_name)[j]:
+    #                     del course1[i]
+    #         student._db.student.update_one(self._condition, {'$set': {'course': course1}})
     def account(self,username,password,wechatnum=''):
         if not (isinstance(username,str) and isinstance(password,str) and isinstance(wechatnum,str)):
             raise  ValueError(username,password,wechatnum)
